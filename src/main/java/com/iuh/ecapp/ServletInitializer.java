3d13b4da534c4faa222644f99eaa4869574adcf0
hieupham20701 <19519511.hieu@student.edu.vn>
2022-09-08 21:14:10 +0700

    initial commit

---
+++ src/main/java/com/iuh/ecapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.iuh.ecapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EcappApplication.class);
+	}
+
+}
---
+++ src/test/java/com/iuh/ecapp/EcappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.ecapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
